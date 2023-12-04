6ab769e531b7d87de6e8699a2b0d7ba262f80820
Github11200 <78944617+Github11200@users.noreply.github.com>
2023-12-03 22:50:00 -0800

    added in hero component with title and everything

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,7 +10,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
     return (
-        <html lang="en">
+        <html lang="en" className="bg-black">
             <body className={inter.className}>{children}</body>
         </html>
     );
