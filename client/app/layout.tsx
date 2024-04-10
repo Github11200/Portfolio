b2a19853768b90b85e4e978592cc2682382250aa
Jinay Patel <78944617+Github11200@users.noreply.github.com>
2024-04-10 20:36:12 +0000

    Added in code for displaying 3d models

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,8 +1,18 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter as FontSans } from "next/font/google"
+import { Lexend } from "next/font/google"
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+import { cn } from "@/lib/utils"
+ 
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+const lexend = Lexend({
+  variable: "--font-lexend",
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -12,11 +22,15 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode; 
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-lexend antialiased",
+          lexend.variable
+        )}>{children}</body>
     </html>
   );
 }
