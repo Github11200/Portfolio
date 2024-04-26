8ab4a264562bb580814b4d6a12f76e1c98d16da4
Jinay Patel <pateljinay@outlook.com>
2024-04-26 21:29:26 +0000

    added in a simple responsive design aspect to make the 3d model disappear

--- components/ui/modelBox.tsx
+++ components/ui/modelBox.tsx
@@ -1,37 +1,55 @@
 "use client";
 
-import { Canvas, useLoader } from "@react-three/fiber";
+import {
+	Canvas,
+	MeshProps,
+	PrimitiveProps,
+	useFrame,
+	useLoader,
+} from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import { Suspense } from "react";
+import { Suspense, useEffect, useRef } from "react";
+import * as THREE from "three";
 
 import { PerspectiveCamera } from "@react-three/drei";
 
 const Model = () => {
-    const gltf = useLoader(GLTFLoader, "/book.glb");
+	const gltf = useLoader(GLTFLoader, "/book.glb");
+	const test = useRef<any>();
 
-    return (
-        <>
-            <primitive object={gltf.scene} scale={0.4} position={[1, 0.3, 0]} />
-        </>
-    );
+	useFrame((state) => {
+		state.camera.updateProjectionMatrix();
+	});
+
+	return (
+		<>
+			<Suspense fallback={null}>
+				<ambientLight intensity={1.2} />
+				<PerspectiveCamera
+					fov={30}
+					aspect={1}
+					near={0.1}
+					far={2000}
+				></PerspectiveCamera>
+				<OrbitControls />
+				<mesh ref={test}>
+					<primitive
+						object={gltf.scene}
+						scale={0.4}
+						position={[1, 0.3, 0]}
+					/>
+				</mesh>
+				<OrbitControls />
+			</Suspense>
+		</>
+	);
 };
 
 export default function MeshComponent() {
-    return (
-        <Canvas>
-            <Suspense fallback={null}>
-                <ambientLight intensity={1.2} />
-                <PerspectiveCamera
-                    fov={100}
-                    aspect={5}
-                    near={0.1}
-                    far={2000}
-                ></PerspectiveCamera>
-                <OrbitControls />
-                <Model />
-                <OrbitControls />
-            </Suspense>
-        </Canvas>
-    );
+	return (
+		<Canvas>
+			<Model />
+		</Canvas>
+	);
 }
