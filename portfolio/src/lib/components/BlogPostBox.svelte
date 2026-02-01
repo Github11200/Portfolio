33c9e8c2ba1cd724bf9e2a0594d4b3c7ee729080
Github11200 <pateljinay@outlook.com>
2026-01-31 23:24:38 -0800

    working on blog post mechanism

---
+++ portfolio/src/lib/components/BlogPostBox.svelte
@@ -0,0 +1,15 @@
+<div class="grid grid-cols-2 rounded-2xl bg-base-200 py-4">
+	<img
+		src="https://img.daisyui.com/images/profile/demo/batperson@192.webp"
+		alt="woah"
+		class="mx-4 rounded-2xl"
+	/>
+	<div class="mx-4 prose mb-4">
+		<h2 class="m-0">Project Name</h2>
+		<p class="mb-0">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum, cupiditate excepturi hic
+			blanditiis voluptatem dignissimos nemo culpa quo perspiciatis qui. Quidem sint officia tempora
+			eaque perspiciatis repellat? Totam, ratione blanditiis.
+		</p>
+	</div>
+</div>
---
+++ portfolio/src/routes/blog/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import BlogPostBox from '$lib/components/BlogPostBox.svelte';
+</script>
+
+<div class="flex h-screen justify-center overflow-y-scroll">
+	<div class="prose mt-16">
+		<h1>Blog Posts</h1>
+		<div class="flex flex-col gap-4">
+			<BlogPostBox />
+			<BlogPostBox />
+			<BlogPostBox />
+			<BlogPostBox />
+		</div>
+	</div>
+</div>
